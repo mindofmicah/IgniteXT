c15fc63751d3de1220b4aa7d6f500219fc32b144
Brian Adams <brian@websiteduck.com>
2011-09-27 22:50:03 -0500

    Code clean up, add function doc

--- system/autoload.php
+++ system/autoload.php
@@ -1,19 +1,24 @@
 <?php
 
-function my_autoload($class)
+/**
+ * Includes a PHP file that should contain the requested class.
+ * 
+ * @param string $class
+ */
+function ignitext_autoload($class)
 {
-  $valid_folders = array('models','libraries');
-  
-  $parts = explode('\\',$class);
-  if (!in_array(strtolower($parts[0]),$valid_folders)) return;
-  
-  $filename = array_pop($parts);
-  
-  $path = implode('/',$parts);
-  $path = str_replace('..','.',$path);
-  $path = strtolower($path);
-  
-  include(APPDIR . $path . '/' . $filename . '.php');
+	$valid_folders = array('models', 'libraries');
+
+	$parts = explode('\\', $class);
+	if (!in_array(strtolower($parts[0]), $valid_folders)) return;
+
+	$filename = array_pop($parts);
+
+	$path = implode('/', $parts);
+	$path = str_replace('..', '.', $path);
+	$path = strtolower($path);
+
+	include APPDIR . $path . '/' . $filename . '.php';
 }
 
-spl_autoload_register('my_autoload');
+spl_autoload_register('ignitext_autoload');
