af331067b958b4949faa70b2c2cbf8784aaa62f4
WebsiteDuck <brian@websiteduck.com>
2011-10-07 10:21:33 -0500

    Add a couple section comments

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /*==============================================================================*
  *  ,--,                                   ,--,                                 *
  *  |  |                                   |  |            ,--,  ,------------, *
@@ -14,20 +14,27 @@
  *     ',---------'                                                             *
  *==============================================================================*/
 
+/**
+ * Configure Paths
+ */
 define('BASEURL','/');
 define('BASEDIR',dirname(__FILE__).'/');
 define('APPDIR',dirname(__FILE__).'/application/');
 define('SYSDIR',dirname(__FILE__).'/system/');
 
 //Sets some PHP configuration options at runtime. Comment this line out if you 
-//want to use the settings from your server's php.ini file.
+//only want to use the settings from your server's php.ini file.
 include(BASEDIR . 'php_settings.php');
 
+/**
+ * Load System Files
+ */
 require(SYSDIR . 'router.php');
 require(SYSDIR . 'controller.php');
 require(SYSDIR . 'model.php');
 require(SYSDIR . 'database.php');
 require(SYSDIR . 'autoload.php');
+require(SYSDIR . 'display.php');
 
 session_cache_limiter('public');
 session_start();
