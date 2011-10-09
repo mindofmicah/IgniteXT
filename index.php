a1334a977e44044d8d2d4bc6dba0c4a781952cde
Brian Adams <brian@websiteduck.com>
2011-10-09 11:54:31 -0500

    Get rid of parens on includes and requires

--- index.php
+++ index.php
@@ -24,17 +24,17 @@ define('SYSDIR',dirname(__FILE__).'/system/');
 
 //Sets some PHP configuration options at runtime. Comment this line out if you 
 //only want to use the settings from your server's php.ini file.
-include(BASEDIR . 'php_settings.php');
+include BASEDIR . 'php_settings.php';
 
 /**
  * Load System Files
  */
-require(SYSDIR . 'router.php');
-require(SYSDIR . 'controller.php');
-require(SYSDIR . 'model.php');
-require(SYSDIR . 'database.php');
-require(SYSDIR . 'autoload.php');
-require(SYSDIR . 'display.php');
+require SYSDIR . 'router.php';
+require SYSDIR . 'controller.php';
+require SYSDIR . 'model.php';
+require SYSDIR . 'database.php';
+require SYSDIR . 'autoload.php';
+require SYSDIR . 'display.php';
 
 session_cache_limiter('public');
 session_start();
