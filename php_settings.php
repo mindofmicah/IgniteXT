131311ea7f1ddbf999db7182cc76f770409431bb
WebsiteDuck <brian@websiteduck.com>
2011-10-10 13:33:11 -0500

    Fix single line comments

--- php_settings.php
+++ php_settings.php
@@ -3,11 +3,11 @@
  * This configuration file sets PHP settings at runtime.
  */
 
-/* Display errors to screen */
+//Display errors to screen
 ini_set('display_errors', 1);
 
-/* Set which errors to display and/or log */
+//Set which errors to display and/or log
 error_reporting(E_ALL ^ E_NOTICE);
 
-/* Enable short PHP open tags */
+//Enable short PHP open tags
 ini_set('short_open_tag', 1);
