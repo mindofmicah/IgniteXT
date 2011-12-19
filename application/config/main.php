8ed4b0eab0172ac4c005ef5e1152c9ec221a4af1
Brian Adams <brian@websiteduck.com>
2011-12-18 21:37:50 -0600

    Move APPID to index and write better comments

--- application/config/main.php
+++ application/config/main.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * Application Identifier
- * This is used to prevent conflicts between multiple IXT applications on the same server.
- * Currently only used in the Session class.
- */
-$IXT->config->application_id = 'my_application';
-
-/**
  * Database Configuration
  * \System\Database::connect(identifier, driver, server, username, password, database);
  */
--- index.php
+++ index.php
@@ -15,21 +15,38 @@
  *==============================================================================*/
 
 /**
- * Define Paths
+ * Define locations of directories used by IgniteXT.
  */
-define('BASEURL','/');
-define('BASEDIR',dirname(__FILE__).'/');
-define('APPDIR',dirname(__FILE__).'/application/');
-define('SHRDIR',dirname(__FILE__).'/shared/');
-define('IXTDIR',dirname(__FILE__).'/ignitext/');
+define('BASEDIR', dirname(__FILE__) . '/');
+define('APPDIR', dirname(__FILE__) . '/application/');
+define('SHRDIR', dirname(__FILE__) . '/shared/');
+define('IXTDIR', dirname(__FILE__) . '/ignitext/');
 
-//Sets some PHP configuration options at runtime. Comment this line out if you 
-//only want to use the settings from your server's php.ini file.
+/**
+ * Define the base URL. This relative URL should point to the location that
+ * contains your index.php and assets folder.
+ */
+define('BASEURL', '/');
+
+/**
+ * Define the application identifier. This will be used by system classes to 
+ * prevent multiple applications from interfering with each other when using 
+ * shared resources such as PHP sessions.
+ */
+define('APPID', 'my_application');
+
+/**
+ * Sets some PHP configuration options at runtime. Comment this line out if you 
+ * only want to use the settings from your server's php.ini file.
+ */
 include BASEDIR . 'php_settings.php';
 
-if (file_exists(APPDIR . 'system/autoload.php')) include APPDIR . 'system/autoload.php';
-elseif (file_exists(SHRDIR . 'system/autoload.php')) include SHRDIR . 'system/autoload.php';
-elseif (file_exists(IXTDIR . 'system/autoload.php')) include IXTDIR . 'system/autoload.php';
+/**
+ * Find and include the autoloader. 
+ */
+if (file_exists(APPDIR . 'system/autoload.php')) require APPDIR . 'system/autoload.php';
+elseif (file_exists(SHRDIR . 'system/autoload.php')) require SHRDIR . 'system/autoload.php';
+elseif (file_exists(IXTDIR . 'system/autoload.php')) require IXTDIR . 'system/autoload.php';
 else throw new Exception('Autoloader not found.');
 
 \System\Event::event( \System\Event_Type::NORMAL, 'IgniteXT', 'Start Application', 'Application has started running.');
