b18192bc3d26e1a064de068f815e46fe38d29c6b
Brian Adams <brian@websiteduck.com>
2011-12-18 20:36:34 -0600

    Make all event's timestamp relative to first event's timestamp

--- ignitext/system/event.php
+++ ignitext/system/event.php
@@ -16,11 +16,13 @@ class Event
 {
 	static $log = array();
 	static $log_events = true;
+	static $start_time = 0;
 
 	function event($type, $from, $action, $description)
 	{
 		if (self::$log_events)
 		{
+			if (!isset(self::$log[0])) self::$start_time = microtime(true);
 			self::$log[] = array(microtime(true), $type, $from, $action, $description);
 		}
 	}
