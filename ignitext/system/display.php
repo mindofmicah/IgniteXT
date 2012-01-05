c28edd7540358765585218c7ada22aacc4d700b5
Brian Adams <brian@websiteduck.com>
2012-01-04 21:04:29 -0600

    Fix parameter spacing

--- ignitext/system/display.php
+++ ignitext/system/display.php
@@ -5,7 +5,7 @@
 namespace System;
 class Display
 {
-	public static function view($file,$data=null)
+	public static function view($file, $data = null)
 	{
 		$requested_file = $file;
 		if (is_array($data)) extract($data);
@@ -24,7 +24,7 @@ class Display
 		echo "View Not Found: " . $requested_file . ".php"; die();
 	}
 
-	public static function return_view($file,$data=null)
+	public static function return_view($file, $data=null)
 	{
 		ob_start();
 		self::view($file,$data);
@@ -33,7 +33,7 @@ class Display
 		return $ret;
 	}
 	
-	public static function template_view($file,$data)
+	public static function template_view($file, $data)
 	{
 		$requested_file = $file;
 		if (is_array($data)) extract($data);
@@ -45,7 +45,7 @@ class Display
 		echo "Template View Not Found: " . $requested_file . ".php"; die();
 	}
 
-	public static function template($title,$file,$data=null,$template='main')
+	public static function template($title, $file, $data = null, $template = 'main')
 	{
 		$data['content_title'] = $title;
 		$data['content_view'] = $file;
