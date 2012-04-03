704d5b9cd666df2c323de7acf15faa9675788423
Brian Adams <brian@websiteduck.com>
2012-04-03 13:19:29 -0500

    Create data object class

---
+++ ignitext/system/classes/data_object.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Data Object
+ * 
+ * An object used for storing data.
+ *
+ * @copyright  Copyright 2011-2012, Website Duck LLC (http://www.websiteduck.com)
+ * @link       http://www.ignitext.com IgniteXT PHP Framework
+ * @license    MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace System\Classes;
+
+class Data_Object
+{
+  
+}
