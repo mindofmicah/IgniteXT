f2da4c893a25d200868f6d4bc454dc0bcf3e1f28
Brian Adams <brian@websiteduck.com>
2012-04-08 03:30:43 -0500

    Create session class unit test, clean up the other tests a bit

--- tests/ignitext/source/libraries/ixt/stopwatch_test.php
+++ tests/ignitext/source/libraries/ixt/stopwatch_test.php
@@ -1,18 +1,20 @@
 <?php
 namespace Libraries\IXT;
 
-class StopwatchTest extends \PHPUnit_Framework_TestCase {
+class Stopwatch_Test extends \PHPUnit_Framework_TestCase {
 
 	protected $stopwatch;
 
-	protected function setUp() {
+	protected function setUp() 
+	{
 		date_default_timezone_set('America/Chicago');
 		$this->stopwatch = new Stopwatch;
 	}
 
 	protected function tearDown() {	}
 
-	public function testMark() {
+	public function test_mark() 
+	{
 		$this->stopwatch->mark('one');
 		$this->stopwatch->mark('two');
 		$marks = $this->stopwatch->get_marks();
@@ -20,7 +22,8 @@ class StopwatchTest extends \PHPUnit_Framework_TestCase {
 		$this->assertArrayHasKey('two',$marks);
 	}
 
-	public function testElapsed_time() {
+	public function test_elapsed_time() 
+	{
 		$this->stopwatch->mark('one');
 		usleep(100000); //tenth of a second
 		$this->stopwatch->mark('two');
