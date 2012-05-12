cd484f67486916febf977c258e63dca195254743
Brian Adams <brian@websiteduck.com>
2012-05-11 22:49:26 -0500

    Fix database config

--- application/config/main.php
+++ application/config/main.php
@@ -36,10 +36,10 @@ if (isset($application_config['databases']))
 		$db_array = explode(',', $db_string);
 		switch (count($db_array))
 		{
-			case 6:	\System\Database::connect($id, $db_array[0], $db_array[1], $db_array[2], $db_array[3], $db_array[4]); break;
-			case 4:	\System\Database::connect_dsn($id, $db_array[0], $db_array[1], $db_array[2]); break;
-			case 3: \System\Database::connect_dsn($id, $db_array[0], $db_array[1]); break;
-			case 2:	\System\Database::connect_dsn($id, $db_array[0]); break;
+			case 5:	\System\Database::connect($id, $db_array[0], $db_array[1], $db_array[2], $db_array[3], $db_array[4]); break;
+			case 3:	\System\Database::connect_dsn($id, $db_array[0], $db_array[1], $db_array[2]); break;
+			case 2: \System\Database::connect_dsn($id, $db_array[0], $db_array[1]); break;
+			case 1:	\System\Database::connect_dsn($id, $db_array[0]); break;
 			default: throw new Exception('Invalid number of parameters in config database settings.'); break;
 		}
 	}
